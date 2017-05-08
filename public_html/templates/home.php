0835b35a03b73a8cc1f0a8b3844a7c36f7b1b74d
Tucker L <tucker@fellafox.com>
2017-05-08 14:13:06 -0600

    Added templates

---
+++ public_html/templates/home.php
@@ -0,0 +1,8 @@
+<section class="welcome">
+	<div class="container">
+		<div class="jumbotron text-center">
+			<h1><i class="fa fa-microchip"></i> Welcome to the Angular Hello World!</h1>
+			<p class="lead orange">Woot woot I did it!</p>
+		</div>
+	</div>
+</section>
